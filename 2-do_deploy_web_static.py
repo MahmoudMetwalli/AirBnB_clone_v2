570cd0ef431b7981f25b90deebf392847a97850c
MahmoudMetwalli <mammkbih@gmail.com>
2024-05-04 22:56:37 +0300

    2024-05-04 10:56:36 PM

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
-"""
-
-from fabric.api import put, run, env
+"""To deploy file on the server"""
 from os.path import exists
-env.hosts = ['142.44.167.228', '144.217.246.195']
+from fabric.operations import put, run, env
+env.hosts = ["54.173.91.227", "3.85.1.149"]
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
+    """To deploy file on the server"""
     if exists(archive_path) is False:
         return False
     try:
@@ -20,11 +16,11 @@ def do_deploy(archive_path):
         put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(path, no_ext))
         run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
+        run('rm -f /tmp/{}'.format(file_n))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -sf {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except:
+    except Exception:
         return False
