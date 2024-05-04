4859aa3d8ce4d838c7ad0183e742381f68480cb6
MahmoudMetwalli <mammkbih@gmail.com>
2024-05-05 00:32:43 +0300

    2024-05-05 12:32:43 AM

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -25,6 +25,7 @@ def do_deploy(archive_path):
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('service nginx restart')
         return True
     except Exception:
         return False
