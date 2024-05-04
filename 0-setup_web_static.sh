482edbf2fe44986e3eed1193058479f657ae3e75
MahmoudMetwalli <mammkbih@gmail.com>
2024-05-04 05:46:54 +0300

    2024-05-04 05:46:54 AM

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,40 +1,12 @@
 #!/usr/bin/env bash
-# Checks and makes dirs and configure nginx
-content="<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>
-" 
+# sets up the web servers for the deployment of web_static
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-if [ ! -d "/data/" ];
-then sudo mkdir -p "/data/"
-fi
-if [ ! -d "/data/web_static/" ];
-then sudo mkdir -p "/data/web_static/"
-fi
-if [ ! -d "/data/web_static/releases/" ];
-then sudo mkdir -p "/data/web_static/releases/"
-fi
-if [ ! -d "/data/web_static/shared/" ];
-then sudo mkdir -p "/data/web_static/shared/"
-fi
-if [ ! -d "/data/web_static/releases/test/" ];
-then sudo mkdir -p "/data/web_static/releases/test/"
-fi
-if [ ! -d "/data/web_static/releases/test/" ];
-then sudo mkdir -p "/data/web_static/releases/test/"
-fi
-
-sudo bash -c " echo $content > '/data/web_static/releases/test/index.html'"
-sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
-sudo chown -RH "ubuntu:ubuntu" "/data/"
-FIND=$(grep "/hbnb_static/" /etc/nginx/sites-enabled/default)
-if [[ -z $FIND ]]; then
-sudo sed -i "40i\	location /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
-fi
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx start
