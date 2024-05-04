0bef0977ed4d50a8964d2ffbf1bddaa9d02f9aa8
MahmoudMetwalli <mammkbih@gmail.com>
2024-05-04 20:57:34 +0300

    2024-05-04 08:57:34 PM

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,12 +2,16 @@
 """To compress webstatic"""
 import datetime
 from fabric.operations import local
+from os.path import isdir
 
 
 def do_pack():
     """Packs webstatic into tgz"""
     try:
-        if local("file versions") != "versions: directory":
+        result = str(local("file versions"))
+        print(result)
+        print(len(result))
+        if isdir("versions") is False:
             local("mkdir versions")
         date = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
         file_path = "versions/web_static_{}.tgz".format(date)
