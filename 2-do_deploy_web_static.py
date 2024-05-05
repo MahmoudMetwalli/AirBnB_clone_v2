c282136a5555bdda8fbc33f1d5073aba0d336942
MahmoudMetwalli <mammkbih@gmail.com>
2024-05-06 02:27:20 +0300

    2024-05-06 02:27:20 AM

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,8 +4,10 @@ Fabric script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
 
-from fabric.api import put, run, env
+
 from os.path import exists
+from fabric.api import put, run, env
+from fabric import exceptions
 env.hosts = ["54.173.91.227", "3.85.1.149"]
 
 
@@ -18,14 +20,16 @@ def do_deploy(archive_path):
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run(f'mkdir -p {path}{no_ext}/')
+        run(f'tar -xzf /tmp/{file_n} -C {path}{no_ext}/')
+        run(f'rm /tmp/{file_n}')
+        run(f'mv {path}{no_ext}/web_static/* {path}{no_ext}/')
+        run(f'rm -rf {path}{no_ext}/web_static')
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run(f'ln -s {path}{no_ext}/ /data/web_static/current')
         run('service nginx restart')
         return True
-    except Exception:
+    except exceptions.CommandTimeout:
+        return False
+    except exceptions.NetworkError:
         return False
