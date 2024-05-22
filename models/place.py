73ef087785595f29aecb94c132b0d4b9fe467fe7
MahmoudMetwalli <mammkbih@gmail.com>
2024-05-22 20:43:14 +0300

    2024-05-22 08:43:14 PM

--- models/place.py
+++ models/place.py
@@ -22,7 +22,7 @@ class Place(BaseModel, Base):
     """class : Place to store more data"""
     if getenv('HBNB_TYPE_STORAGE') == "db":
         __tablename__ = "places"
-        city_id = Column(String(60), ForeignKey("users.id"),
+        city_id = Column(String(60), ForeignKey("cities.id"),
                          nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"),
                          nullable=False)
